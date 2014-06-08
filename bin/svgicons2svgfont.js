c23055bd0182b88521e47248856e5fe82f179f7d
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-06-08 10:18:24 +0200

    Adding command line tools

---
+++ bin/svgicons2svgfont.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+var svgfont2svgicons = require(__dirname + '/../src/index.js')
+  , Fs = require('fs')
+  , path = require('path')
+;
+
+var fontStream = Fs.createReadStream(process.argv[2]);
+var iconProvider = svgfont2svgicons();
+
+fontStream.pipe(iconProvider);
+
+iconProvider.on('readable', function() {
+  var glyph, steam, glyphPath;
+  while(null !== glyph) {
+    glyph = iconProvider.read();
+    if(glyph) {
+      glyphPath = path.join(process.argv[3], glyph.name + '.svg');
+      console.log('Saving glyph "' + glyph.name + '" to "' + glyphPath + '"');
+      glyph.stream.pipe(Fs.createWriteStream(glyphPath));
+    }
+  }
+});
+
