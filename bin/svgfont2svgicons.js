d9c75e0ddab90f8ad91d8937e5c0651c99735785
Martin Pedersen <martin@m4r7.in>
2015-07-04 16:44:11 +0000

    Fixed 'cant read property pipe of undefined', added option --namemap
    --namemap is a json file that maps unicode to icon name
    Useful when reconstructing font awesome
    
    Example name map:
    {
        "\uF167": "youtube",
        "\uF16A": "youtube-play",
        "\uF166": "youtube-square"
    }

--- bin/svgfont2svgicons.js
+++ bin/svgfont2svgicons.js
@@ -1,25 +1,63 @@
 #! /usr/bin/env node
 
-var svgfont2svgicons = require(__dirname + '/../src/index.js')
-  , Fs = require('fs')
-  , path = require('path')
-;
+var svgfont2svgicons = require(__dirname + '/../src/index.js'),
+  Fs = require('fs'),
+  path = require('path');
 
-var fontStream = Fs.createReadStream(process.argv[2]);
+var srcFile,
+  destPath,
+  options = {};
+
+for (var i = 2; i < process.argv.length; i++) {
+  switch (process.argv[i]) {
+    case '--namemap':
+      options.namemap = JSON.parse(Fs.readFileSync(process.argv[++i]));
+      break;
+    case '--help':
+    case '--usage':
+      console.log([
+        'node ' + process.argv[2] + ' <source svg file> <destination path>',
+        '  --namemap <json file>'
+      ].join('\n'));
+      break;
+    default:
+      if (!srcFile)
+        srcFile = process.argv[i];
+      else if (!destPath)
+        destPath = process.argv[i];
+  }
+}
+
+if (!srcFile || !destPath)
+  console.log('see --usage');
+
+var fontStream = Fs.createReadStream(srcFile);
 var iconProvider = svgfont2svgicons();
 var unamedIconCount = 0;
 
 fontStream.pipe(iconProvider);
 
 iconProvider.on('readable', function() {
-  var glyph, steam, glyphPath;
-  while(null !== glyph) {
-    glyph = iconProvider.read();
-    if(glyph) {
-      glyphPath = path.join(process.argv[3], glyph.name + '.svg');
-      console.log('Saving glyph "' + glyph.name + '" to "' + glyphPath + '"');
-      glyph.stream.pipe(Fs.createWriteStream(glyphPath));
+  var glyph;
+  while((glyph = iconProvider.read()) !== null) {
+    var glyphName = glyph.name,
+      readableHex = glyph.metadata.unicode.map(function(str) {
+        return '\\u' + str.charCodeAt(0).toString(16).toUpperCase();
+      }).join('');
+
+    if (options.namemap) {
+      var unicode = glyph.metadata.unicode.join('');
+      glyphName = options.namemap[unicode];
     }
+
+    if (!glyphName) {
+      console.log('Missing glyph name for unicode', readableHex);
+      continue;
+    }
+
+    var glyphPath = path.join(destPath, glyphName + '.svg');
+    console.log('Saving glyph "' + glyphName + '" to "' + glyphPath + '"');
+    glyph.pipe(Fs.createWriteStream(glyphPath));
   }
 });
 
